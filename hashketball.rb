4876dbb6554650e82453e88a40ca450ea5ac06a5
Ian Davies <mr.daviesian@gmail.com>
2021-01-31 00:33:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,7 +49,7 @@ def game_hash
           :slam_dunks =>
         },
         {
-          :player_name => ""
+          :player_name => "Jason Terry"
           :number =>
           :shoe =>
           :points =>
