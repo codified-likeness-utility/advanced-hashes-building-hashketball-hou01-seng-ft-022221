a71cc7623813ffc1c228f011bcf09ade098d4b48
Ian Davies <mr.daviesian@gmail.com>
2021-01-31 00:09:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,12 @@
 def game_hash
   {
     :home => {
-      :team_name => {
-        :colors => {},
+                :team_name => {},
+                :colors => {}
+    },
     :away => {
-      :team_name => {
-        :colors => {}
+                :team_name => {},
+                :colors => {}
+    }
   }
 end
